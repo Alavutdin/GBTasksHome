700a55d29588b8f683406ac03cdef93a0bbc331b
Alavutdin <alavutdin_atlan@mail.ru>
2023-03-12 23:35:32 +0300

    Task2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('Привет')
+""""Задача 2: Найдите сумму цифр трехзначного числа.
+
+*Пример:*
+
+123 -> 6 (1 + 2 + 3)
+100 -> 1 (1 + 0 + 0) |"""
+
+numberStart = int(input("Введите трехзначное число: "))
+numberA= int(numberStart/100)%10
+numberB=int(numberStart/10)%10
+numberC = numberStart%10
+print(numberA+numberB+numberC)
